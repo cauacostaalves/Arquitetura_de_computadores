6f414f01654a8db384de4c3f5cda85b6f85eb68a
Cauã Costa Alves <cauacostalves@gmail.com>
2024-09-11 12:03:17 -0300

    att

--- GUIAS/Guia 7/Codigos/VeriLog/Guia_0702.v
+++ GUIAS/Guia 7/Codigos/VeriLog/Guia_0702.v
@@ -1,35 +1,38 @@
 // -------------------------
-// Guia_0701 - AND e NAND
+// Guia_0702 - OR e NOR
 // Nome: Cauã Costa Alves
 // Matricula: 855926
 // -------------------------
 
 module LU (
     output wire Y,          // Saída selecionada
-    output wire AND_Out,    // Saída AND
-    output wire NAND_Out,   // Saída NAND
+    output wire OR_Out,     // Saída OR
+    output wire NOR_Out,    // Saída NOR
     input wire A,           // Entrada A
     input wire B,           // Entrada B
     input wire Sel          // Sinal de Seleção
 );
 
 // Descrição das operações lógicas
-wire and_result;
-wire nand_result;
+wire or_result;
+wire nor_result;
 
-// Operações AND e NAND
-and AND1 (and_result, A, B);
-nand NAND1 (nand_result, A, B);
+// Operações OR e NOR
+or OR1 (or_result, A, B);
+nor NOR1 (nor_result, A, B);
 
 // Multiplexador para selecionar a saída
-assign Y = Sel ? and_result : nand_result;
+assign Y = Sel ? or_result : nor_result;
 
 // Saídas independentes
-assign AND_Out = and_result;
-assign NAND_Out = nand_result;
+assign OR_Out = or_result;
+assign NOR_Out = nor_result;
 
 endmodule // LU
 
+// -------------------------
+// Banco de Testes para LU
+// -------------------------
 module test_LU;
 
 // Definir dados
@@ -37,14 +40,14 @@ reg x;
 reg y;
 reg s;
 wire z;
-wire and_out;
-wire nand_out;
+wire or_out;
+wire nor_out;
 
 // Instanciar o módulo LU
 LU modulo (
     .Y(z),
-    .AND_Out(and_out),
-    .NAND_Out(nand_out),
+    .OR_Out(or_out),
+    .NOR_Out(nor_out),
     .A(x),
     .B(y),
     .Sel(s)
@@ -53,12 +56,12 @@ LU modulo (
 // Parte principal
 initial begin : main
     // Exibir cabeçalho
-    $display("Guia_0701 - Cauã Costa Alves - 855926");
-    $display("     x    y    s    z  and_out  nand_out");
+    $display("Guia_0702 - Cauã Costa Alves - 855926");
+    $display("     x    y    s    z  or_out  nor_out");
 
     // Aplicar estímulos e monitorar resultados
     x = 1'b0; y = 1'b0; s = 1'b0;
-    #1 $monitor("  %4b %4b %4b %4b %4b   %4b", x, y, s, z, and_out, nand_out);
+    #1 $monitor("  %4b %4b %4b %4b %4b   %4b", x, y, s, z, or_out, nor_out);
     
     #1 x = 1'b0; y = 1'b1; s = 1'b0; 
     #1 s = 1'b1;                     
